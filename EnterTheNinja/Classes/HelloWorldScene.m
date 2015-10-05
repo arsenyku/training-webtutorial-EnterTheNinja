77f9315ff4a37256e8600699f3de43f6e4872b8b
asu <arsenyk.ustaris.dev@gmail.com>
2015-10-05 12:47:20 -0700

    Spinning ninja, monsters spawn every 1.5s

--- EnterTheNinja/Classes/HelloWorldScene.m
+++ EnterTheNinja/Classes/HelloWorldScene.m
@@ -14,34 +14,82 @@
 
 // -----------------------------------------------------------------------
 
+@interface HelloWorldScene()
+@property (nonatomic, strong) CCSprite *player;
+@end
+
+
 @implementation HelloWorldScene
 
 // -----------------------------------------------------------------------
 
 - (id)init
 {
-    // Apple recommend assigning self with supers return value
+    // 2
     self = [super init];
+    if (!self) return(nil);
     
-    // The thing is, that if this fails, your app will 99.99% crash anyways, so why bother
-    // Just make an assert, so that you can catch it in debug
-    NSAssert(self, @"Whoops");
+    // 3
+    self.userInteractionEnabled = YES;
     
-    // Background
-    CCSprite9Slice *background = [CCSprite9Slice spriteWithImageNamed:@"white_square.png"];
-    background.anchorPoint = CGPointZero;
-    background.contentSize = [CCDirector sharedDirector].viewSize;
-    background.color = [CCColor grayColor];
+    // 4
+    CCNodeColor *background = [CCNodeColor nodeWithColor:[CCColor lightGrayColor]];     //    [CCColor colorWithRed:0.2f green:0.2f blue:0.2f alpha:1.0f]];
     [self addChild:background];
     
-    // The standard Hello World text
-    CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"ArialMT" fontSize:64];
-    label.positionType = CCPositionTypeNormalized;
-    label.position = (CGPoint){0.5, 0.5};
-    [self addChild:label];
+    // 5
+    _player = [CCSprite spriteWithImageNamed:@"player.png"];
+    _player.position  = ccp(self.contentSize.width/2,self.contentSize.height/2);
+    [self addChild:_player];
     
-    // done
+    // 6
+    CCActionRotateBy* actionSpin = [CCActionRotateBy actionWithDuration:1.5f angle:360];
+    [_player runAction:[CCActionRepeatForever actionWithAction:actionSpin]];
+    
+    // 7
+    CCButton *backButton = [CCButton buttonWithTitle:@"[ Menu ]" fontName:@"Verdana-Bold" fontSize:18.0f];
+    backButton.positionType = CCPositionTypeNormalized;
+    backButton.position = ccp(0.85f, 0.95f); // Top Right of screen
+    [backButton setTarget:self selector:@selector(onBackClicked:)];
+    [self addChild:backButton];
+    
+    [self addMonster:1];
+    
     return self;
+    
+
+
+}
+
+
+- (void)addMonster:(CCTime)dt {
+    
+    CCSprite *monster = [CCSprite spriteWithImageNamed:@"monster.png"];
+    
+    // 1
+    int minY = monster.contentSize.height / 2;
+    int maxY = self.contentSize.height - monster.contentSize.height / 2;
+    int rangeY = maxY - minY;
+    int randomY = (arc4random() % rangeY) + minY;
+    
+    // 2
+    monster.position = CGPointMake(self.contentSize.width + monster.contentSize.width/2, randomY);
+    [self addChild:monster];
+    
+    // 3
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangeDuration = maxDuration - minDuration;
+    int randomDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    // 4
+    CCAction *actionMove = [CCActionMoveTo actionWithDuration:randomDuration position:CGPointMake(-monster.contentSize.width/2, randomY)];
+    CCAction *actionRemove = [CCActionRemove action];
+    [monster runAction:[CCActionSequence actionWithArray:@[actionMove,actionRemove]]];
+}
+
+-(void)onEnter{
+    [super onEnter];
+    [self schedule:@selector(addMonster:) interval:1.5];
 }
 
 // -----------------------------------------------------------------------
